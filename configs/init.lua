c77dd6d57f0fd89bb6b2bc0e16dc2a412e981d00
Itay <31004688+irussak@users.noreply.github.com>
2025-08-18 13:28:30 +0300

    Update nvim configurations

--- configs/init.lua
+++ configs/init.lua
@@ -1,11 +1,14 @@
 -- ~/.config/nvim/init.lua
 
+-- Leader must be set before plugins load
 vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
--- Load Lazy
-require("lazy-plugins")
-
--- Load plugin configs
-require("config")
+-- 1) Core editor options (single source of truth)
 require("options")
 
+-- 2) Plugin manager + specs (plugins live under lua/lazy-plugins/)
+require("lazy-plugins")
+require("config")
+-- No global "config" require here; each plugin spec should do:
+-- config = function() require("config.<plugin>") end
